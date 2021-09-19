20a18baec6c60f966d2e5ba4aace8bcc47a5a65c
AlvaroSbsn <78981404+AlvaroSbsn@users.noreply.github.com>
2021-09-19 18:46:29 +0200

    6.1 + 6.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,4 +30,9 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(collider other)
+    {
+        other.gameObjetct.SetActive(false);
+    }
 }
